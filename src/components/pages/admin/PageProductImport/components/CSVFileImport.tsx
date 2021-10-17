822e8a1afdbbb7c62d5589cdecdd7cbae7a5f7f0
Gleb Stashinsky <g.stashinsky@gmail.com>
2021-10-17 16:46:46 +0300

    feat: add configuration for JWT

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,18 +31,25 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async (e: any) => {
       // Get the presigned URL
+      localStorage.setItem('authorization_token', 'Z2xvYnVzMDA3OlRFU1RfUEFTU1dPUkQ=');
+      const authorization_token = localStorage.getItem('authorization_token');
+
       const response = await axios({
-        method: 'GET',
-        url,
-        params: {
-          name: encodeURIComponent(file.name)
-        }
+          headers: {
+              'Authorization': `Basic ${authorization_token}`
+          },
+          method: 'GET',
+          url,
+          params: {
+              name: encodeURIComponent(file.name)
+          }
       })
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
       const result = await fetch(response.data, {
-        method: 'PUT',
-        body: file
+          headers: {'Content-Type': 'text/csv'},
+          method: 'PUT',
+          body: file
       })
       console.log('Result: ', result)
       setFile('');
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@
 const API_PATHS = {
   product: 'https://newp4jdf84.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://aau3s2yy4k.execute-api.eu-west-1.amazonaws.com/dev/',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://aau3s2yy4k.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://newp4jdf84.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
